7c700ee4f3084aff654230a0c4a334aad457f17a
Ayat <alkayedayat93@gmail.com>
2022-04-12 05:54:16 +0300

    01

--- trees/trees/trees.py
+++ trees/trees/trees.py
@@ -53,8 +53,33 @@ class BinaryTree:
 
 class Binary_search_tree(BinaryTree):
     
-    def add(self):
-        pass
+    def add(self , value):
+            '''root --> left -- > right'''
+            
+            def _add(node):
+                
+                if node:
+                    # print(node.value)
+                    if value >= node.value:
+                        new_root = TNode(value , None , node)
+                        print(node.value)
+                        
+
+                    if value > node.left.value :
+                        _add(node.right)
+                        if value > node :
+                            new_node = TNode(value , None , node)
+                            print('hi')
+                        
+                    _add(node.left)
+
+                    _add(node.right)
+
+            _add(self.root)
+
+
+
+
 
     def Contains(self):
         pass
@@ -66,26 +91,36 @@ class Binary_search_tree(BinaryTree):
 
 
 if __name__ == '__main__':
-    
-    node1 = TNode('A')
-    node2 = TNode('B')
-    node3 = TNode('C')
-    node4 = TNode('D')
-    node5 = TNode('E')
-    node6=TNode('F')
-    node7=TNode('G')
+
+    node1 = TNode(60)
+    node2 = TNode(30)
+    node3 = TNode(50)
+    node4 = TNode(10)
+    node5 = TNode(20)
+    node6=TNode(40)
+    # node7=TNode(7)
+    # node1 = TNode('A')
+    # node2 = TNode('B')
+    # node3 = TNode('C')
+    # node4 = TNode('D')
+    # node5 = TNode('E')
+    # node6=TNode('F')
+    # node7=TNode('G')
     node1.left = node2
     node1.right = node3
     node2.left = node4
     node2.right= node5
     node3.left= node6
     # node3.right = node7
-
+    
 
     tree = BinaryTree()
     tree.root = node1
 
-    tree.Pre_order_rec()
+    binary_search_tree = Binary_search_tree()
+    binary_search_tree.add(35)
+
+    # tree.Pre_order_rec()
     # tree.In_order_rec()
     # tree.Post_ord_rec()
 
